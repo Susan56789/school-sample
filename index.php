f92cf0e84b85449bf0240e4d58e69ee1757a2b25
sueNimoh <mwihaki2susan@gmail.com>
2021-02-13 11:29:01 +0300

    Sample school website

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE>
+<html>
+<head>
+<meta charset="utf-8" />
+        <link rel="stylesheet" href="index-style.css" />
+<title>UWEZO ACADEMY</title>
+</head>
+<body>
+<div id="main_wrapper">
+            <header>
+                <div id="main_title">
+                    <img src="images/logo.jpg" alt="Uwezo Academy" id="logo" />
+                    <h1>UWEZO ACADEMY</h1>
+                    <h2>Your choices,Your Future</h2>
+                </div>
+                
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="enroll.php">Enroll</a></li>
+                        <li><a href="news.php">News</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div id="banner_image">
+               
+            </div>
+            
+            <section>
+                <article>
+                    <h1><img src="images/a.jpg"  width="400px" height="250px"alt="Uwezo Academy" class="ico_categorie" /><br/>Welcome to Uwezo Academy..</h1>
+                    <p>Learn with us</p>
+                    <p>Grow your academic and non-academic skills</p>
+                    <p></p>
+                </article>
+                <aside>
+                    <h1>Uwezo Academy</h1>
+                    <img src="a.jpg" alt="" id="arrow" />
+                    <p>The school was founded in 2017. We are located at Uwezo town in Matope county.Safe
+             environment for your children to learn and grow intellectually.</p>
+                    <p>Our staff are fully committed to ensure each student achieve the best of their potential.
+                    Over the years we have produced top students during the national examinations.</p>
+                  </aside>
+            </section>
+            
+            <footer>
+                <div id="odel">
+                    <h1>Odel</h1>
+                    <p><a href="">Student</a></p>
+                    <p><a href="">Staff</a></p>
+                </div>
+                <div id="my_gallery">
+                    <h1>Gallery</h1>
+                    <p><img src="images/s.jpg" alt="Picture" /><img src="images/s2.jpg" alt="Picture" /><img src="images/s3.jpg" alt="Picture" /><img src="images/s4.jpg" alt="Picture" /></p>
+                </div>
+                <div id="my_address">
+                    <h1>Address</h1>
+                    <address>
+                        <p>+2547222</p>
+                        <p>P.O BOX 2496-108</p>
+                        <p>Uwezo Town,Matope County</p>
+                        
+</address>
+                    <ul>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Youtube</a></li>
+                        <li><a href="#">Facebook</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+</body>
+</html>
